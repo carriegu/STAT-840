9f0dc3233c506bf74542de3ba0957490b97913f8
mlysy <mlysy@users.noreply.github.com>
2020-04-12 22:37:07 -0400

    added rstan and TMB functionality

--- DMRegression/R/DMRegression-package.R
+++ DMRegression/R/DMRegression-package.R
@@ -2,9 +2,11 @@
 #'
 #' A DESCRIPTION OF THE PACKAGE
 #'
+#' @rawNamespace useDynLib(DMRegression, .registration=TRUE); useDynLib(DMRegression_TMBExports)
 #' @import methods
 #' @import Rcpp
 #' @import dirmult
+#' @import TMB
 #' @importFrom rstan sampling
 #'
 #' @references
